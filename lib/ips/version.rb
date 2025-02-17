15e86141ce814d6d3715f6c0a675984a0cc51051
Vanessa Fotso <vfotso@mitre.org>
2025-02-16 22:21:32 -0500

    bump kit version
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/ips/version.rb
+++ lib/ips/version.rb
@@ -1,4 +1,4 @@
 module IPS
-  VERSION = '0.10.2'.freeze
-  LAST_UPDATED = '2025-02-12'.freeze # TODO: update next release
+  VERSION = '0.11.0'.freeze
+  LAST_UPDATED = '2025-02-17'.freeze # TODO: update next release
 end
